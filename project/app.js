81e8cabee23afa64522988c980879125fbfe7854
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2024-08-19 11:43:59 +0900

    6.3 미들웨어 사용실습

--- project/app.js
+++ project/app.js
@@ -1,23 +1,29 @@
-const express = require('express');
-const path = require('path'); // 경로처리
+const express = require("express");
+const path = require("path"); // 경로처리
 
 const app = express();
 
 // 변수 설정(port 변수에 3000 설정)
-app.set('port', process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3000);
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-})
+// 미들웨어 사용
+app.use((req, res, next) => {
+  console.log("이 코드는 모든 요청에 실행합니다.");
+  next(); // 다음라우터 찾기
+});
 
-app.get('/main', (req, res) => {
-    res.send('hello main');
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
-app.get('/login', (req, res) => {
-    res.send('hello login');
-})
+app.get("/main", (req, res) => {
+  res.send("hello main");
+});
 
-app.listen(app.get('port'), ()=>{
-    console.log('서버 실행');
-})
+app.get("/login", (req, res) => {
+  res.send("hello login");
+});
+
+app.listen(app.get("port"), () => {
+  console.log("서버 실행");
+});
