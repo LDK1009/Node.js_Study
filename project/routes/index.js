38af7ee9fef518b5f9d826a3a964c480362a9615
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2024-08-20 10:31:21 +0900

    6.11 라우터 그룹화

--- project/routes/index.js
+++ project/routes/index.js
@@ -1,10 +1,14 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
 // GET / 라우터
-router.get('/', (req,res)=>{
-    res.send('Hello, Root');
-});
+router.route("/")
+  .get((req, res) => {
+    res.send("Hello, Root get");
+  })
+  .post((req, res) => {
+    res.send("Hello, Root post");
+  });
 
 module.exports = router;
